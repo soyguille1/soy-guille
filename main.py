699751869f211cb64fefcb276d856efebaa08e65
Laboratorio <Laboratorio@unida.edu.py>
2023-01-25 21:46:05 +0100

    mian y login

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from login import login
+
+app = Flask(__name__)
+
+    ##servicios rest
+
+app.register_blueprint(login)
+@app.route('/', methods=['GET'])
+def hello():
+    return 'Este es una prueba'
+
+if __name__ == "__main__":
+  
+    app.run(host = '0.0.0.0', debug = True, port = 5001)
+    app.run(debug = True)
